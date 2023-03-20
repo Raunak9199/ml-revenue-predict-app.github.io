a7d6966d191ab837e8fb6994ac7ef81d9a893570
raunak9199 <raunakraj94543@gmail.com>
2023-03-20 11:19:17 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import pickle
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    # Get the uploaded file from the HTML form
+    csv_file = request.files['csv_file']
+    
+    # Read the CSV file into a Pandas DataFrame
+    df = pd.read_csv(csv_file)
+    
+    # Convert the DataFrame into an HTML table
+    table_html = df.to_html(index=False)
+    
+    # Pass the HTML table to the template
+    return render_template('csv_display.html', table_html=table_html)
+
+
+@app.route('/train_model', methods=['POST'])
+def train_model():
+    csv_data = request.files['csv_file']
+    df = pd.read_csv(csv_data)
+    # df.head()
+    x = df['Temperature']
+    y = df['Revenue']
+
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
+
+    model=LinearRegression()
+
+    np.array([x_train]).ndim
+
+    model.fit(np.array([x_train]).reshape(-1,1), y_train)
+
+    y_pred = model.predict(np.array(x_test).reshape(-1,1))
+    # y_pred
+    score = r2_score(y_test,y_pred)
+    return render_template('result.html', total_score=f'Score is: {score}')
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    prediction = model.predict([[request.form.get('temperature')]])
+    output = round(prediction[0],2)
+    print(output)
+    return render_template("index.html", prediction_text = f"Total revenue generated id Rs. {output}/-")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
